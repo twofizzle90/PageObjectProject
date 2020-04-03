f08772767f43d0e2c4b4a9beade9938d3da1d13c
Tofazzal Hiro <hossaincarver09@gmail.com>
2020-04-03 17:17:09 -0400

    first push

---
+++ PageObjectPractice/src/test/LoginTest.java
@@ -0,0 +1,65 @@
+package test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
+import pages.HomePage;
+
+public class LoginTest {
+	private WebDriver driver;
+	private String baseUrl;
+	ExtentReports report;
+	ExtentTest test;
+	HomePage hp;
+
+	@BeforeClass
+	public void beforeClass() {
+		System.setProperty("webdriver.gecko.driver", "/Users/tofazzalhiro/Seleniumfiles/browserDriver/geckodriver");
+		baseUrl = "http://www.letskodeit.com/";
+		report = new ExtentReports("//Users//tofazzalhiro//Desktop//pageobjecttest.html");
+		test = report.startTest("Verify Welcome Text");
+		driver = new FirefoxDriver();
+		hp = new HomePage(driver, test);
+		test.log(LogStatus.INFO, "Browser Started...");
+
+		// Maximize the browser's window
+		driver.manage().window().maximize();
+		test.log(LogStatus.INFO, "Browser Maximized");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get(baseUrl);
+		test.log(LogStatus.INFO, "Web application opened");
+	}
+	
+	@Test
+	public void test1_validLoginTest() throws Exception {
+		//hp.login("test@email.com", "abcabc");
+		Thread.sleep(7000);
+		driver.findElement(By.id("comp-jgmoxws4")).click();
+		hp.clickLoginLink();
+		
+		Thread.sleep(3000);
+		
+		boolean result = hp.isWelcomeTextPresent();
+
+		Assert.assertTrue(result);
+		test.log(LogStatus.PASS, "Verified Welcome Text");
+	}
+	
+	@AfterClass
+	public void afterClass() {
+		//driver.quit();
+		report.endTest(test);
+		report.flush();
+	}
+}
